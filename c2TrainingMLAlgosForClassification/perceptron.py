b85ed0ebc9f5ad02ec4b2d3d7d9645b76699bbd2
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-13 18:59:51 -0400

    Initial commit

---
+++ c2TrainingMLAlgosForClassification/perceptron.py
@@ -0,0 +1,267 @@
+from matplotlib.colors import ListedColormap
+from numpy.random import seed
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+class Perceptron(object):
+    '''Perceptron classifier.
+    Parameters------------------------------------
+    eta: float     Learning rate (on [0, 1])
+    n_iter: int    Iterations over training data set
+
+    Attributes------------------------------------
+    w_: 1d-array   Weights after fitting
+    errors_: list  Number of misclassifications after every iteration
+    '''
+
+    def __init__(self, eta = 0.01, n_iter = 10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        '''Fit training data.
+        Parameters--------------------------------
+        X: {array-like}, shape = [n_samples, n_features]
+             Training vectors where n_samples is the number of samples and
+             n_features is the number of features
+        y: array-like, shape = [n_samples] Target values
+
+        Returns-----------------------------------
+        self: object
+        '''
+
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+
+            self.errors_.append(errors)
+
+        return self
+
+    def net_input(self, X):
+        '''Calculate net input'''
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        '''Return class label after unit step'''
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+
+class AdalineSGD(object):
+    '''Adaptive Linear Neuron Classifier
+    Params----------------------------------------
+    eta: float     Learning rate (on [0, 1])
+    n_iter: int    Number of training iterations
+
+    Attributes------------------------------------
+    w_: 1d-array      Weights after fitting
+    errors_: list     No. of misclassifications at each iteration
+    shuffle: bool     Shuffles training data on each iter if True
+    random_state: int Set random state for shuffling and initializing weights
+    '''
+    def __init__(self,
+                 eta = 0.01,
+                 n_iter = 10,
+                 shuffle = True,
+                 random_state = None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, X, y):
+        '''Fit training data.
+        Params------------------------------------
+        X: {array-like}, shape = [n_samples, n_features]    Training vectors
+        y: array-like, shape = [n_samples]                  Target values
+
+        Returns-----------------------------------
+        self: object
+        '''
+        self._initialize_weights(X.shape[1])
+        #self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+
+    def partial_fit(self, X, y):
+        '''Fit training data w/o reinitializing weights'''
+        if not self.w_initialized:
+            self._initialie_weights(X.shape[1])
+
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        '''Shuffle training data'''
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        '''Initialize weights to 0s'''
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        '''Apply Adaline learning rule to update weights'''
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
+    
+    def net_input(self, X):
+        '''Calculate net input'''
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        '''Compute linear activation'''
+        return self.net_input(X)
+
+    def predict(self, X):
+        '''Return class label after unit step'''
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+    
+                                
+
+iris_url = ('https://raw.githubusercontent.com/pydata/pandas/' + 
+            'master/pandas/tests/data/iris.csv')
+df = pd.read_csv(iris_url)
+print df.tail()
+
+y = df.iloc[0:100, 4].values   # R: df[1:100, 4] # Flower names
+y = np.where(y == 'Iris-setosa', -1, 1) # covert to numeric labels
+
+X = df.iloc[0:100, [0, 2]].values # R: df[1:100, c(1, 3)] # Sepal & Petal len
+
+# Plot SepalLength vs PetalLength
+# R: plot(X[1:50, 1], X[1:50, 2], col = 2, pch = 'o')
+plt.scatter(
+    X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
+plt.scatter(X[50:100, 0],
+            X[50:100, 1],
+            color = 'blue',
+            marker = 'x',
+            label = 'versicolor')
+plt.xlabel('Petal length')
+plt.ylabel('Sepal length')
+plt.legend(loc = 'upper left')
+plt.show()
+
+ppn = Perceptron(eta = 0.1, n_iter = 10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
+plt.xlabel('Iterations')
+plt.ylabel('n misclassified')
+plt.show()
+
+
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    # Set up marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'grey', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # Plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # Plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x = X[y == cl, 0],
+                    y = X[y == cl, 1],
+                    alpha = 0.8,
+                    c = cmap(idx),
+                    marker = markers[idx],
+                    label = cl)
+
+plot_decision_regions(X, y, classifier = ppn)
+plt.xlabel('Sepal length (cm)')
+plt.ylabel('Petal length (cm)')
+plt.legend(loc = 'upper left')
+plt.show()
+
+'''
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (8, 4))
+
+ada1 = AdalineGD(n_iter = 10, eta = 0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker = 'o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate: 0.01')
+
+ada2 = AdalineGD(n_iter = 10, eta = 0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker = 'o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate: 0.001')
+plt.show()
+'''
+
+X_std = np.copy(X)
+X_std [:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std [:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+'''
+ada = AdalineGD(n_iter = 15, eta = 0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier = ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length (Z)')
+plt.ylabel('petal length (Z)')
+plt.legend(loc = 'upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('SSE')
+plt.show()
+'''
+
+ada = AdalineSGD(n_iter = 15, eta = 0.01, random_state = 1)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier = ada)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('Sepal length (Z)')
+plt.ylabel('Petal length (Z)')
+plt.legend(loc = 'upper left')
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker = 'o')
+plt.xlabel('Iterations')
+plt.ylabel('Mean cost')
+plt.show()
+           
