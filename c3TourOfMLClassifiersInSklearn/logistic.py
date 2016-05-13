ec49f2d9d5451c58bf73a8d5e68534866a759c4a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-13 19:33:10 -0400

    Added logistic regression model and plot to c3/logistic.py

--- c3TourOfMLClassifiersInSklearn/logistic.py
+++ c3TourOfMLClassifiersInSklearn/logistic.py
@@ -1,3 +1,8 @@
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,3 +21,74 @@ plt.ylim(-0.1, 1.1)
 plt.xlabel('z')
 plt.ylabel('$\phi (z)$')
 plt.show()
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+(X_train, X_test, y_train, y_test) = train_test_split(
+    X, y, test_size = 0.3, random_state = 0)
+
+sc = StandardScaler()
+# Scale data
+sc.fit(X_train) # Use same to transform both train and test sets
+X_train_std = sc.transform(X_train)
+X_test_std  = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+def plot_decision_regions(
+    X, y, classifier, test_idx = None, resolution = 0.02):
+
+    # Set up marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'grey', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
+    # plot all samples
+    X_test, y_test = X[test_idx, :], y[test_idx]
+    
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x = X[y == cl, 0],
+                    y = X[y == cl, 1],
+                    alpha = 0.8,
+                    c = cmap(idx),
+                    marker = markers[idx],
+                    label = cl)
+        
+    # highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c = '',
+                    alpha = 1.0,
+                    linewidth = 1,
+                    marker = 'o',
+                    s = 55,
+                    label = 'test set')
+        
+
+lr = LogisticRegression(C = 1000.0, random_state = 0)
+lr.fit(X_train_std, y_train)
+
+plot_decision_regions(
+    X_combined_std, y_combined, classifier = lr, test_idx = range(105, 150))
+plt.xlabel('Petal length (Z)')
+plt.ylabel('Petal width (Z)')
+plt.legend(loc = 'upper left')
+plt.show()
+
+# Predict probabilities
+print lr.predict_proba(X_test_std[0, :]) # prob of each class
