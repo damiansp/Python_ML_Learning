b607e4b58eb0600cc36a6b36e0e1dc46057c3e2e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-18 18:17:24 -0400

    added *.pyc to .gitignore

--- c3TourOfMLClassifiersInSklearn/logistic.py
+++ c3TourOfMLClassifiersInSklearn/logistic.py
@@ -5,6 +5,10 @@ from sklearn.preprocessing import StandardScaler
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                          '../'))
+from plot_decision_regions import plot_decision_regions
 
 def sigmoid(z):
     return 1. / (1. + np.exp(-z))
@@ -37,48 +41,6 @@ X_test_std  = sc.transform(X_test)
 X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
 
-def plot_decision_regions(
-    X, y, classifier, test_idx = None, resolution = 0.02):
-
-    # Set up marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'grey', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-    
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-    
-    # plot all samples
-    X_test, y_test = X[test_idx, :], y[test_idx]
-    
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x = X[y == cl, 0],
-                    y = X[y == cl, 1],
-                    alpha = 0.8,
-                    c = cmap(idx),
-                    marker = markers[idx],
-                    label = cl)
-        
-    # highlight test samples
-    if test_idx:
-        X_test, y_test = X[test_idx, :], y[test_idx]
-        plt.scatter(X_test[:, 0],
-                    X_test[:, 1],
-                    c = '',
-                    alpha = 1.0,
-                    linewidth = 1,
-                    marker = 'o',
-                    s = 55,
-                    label = 'test set')
-        
 
 # The param C below = 1/lambda for L2 normalization --
 # as C++ -> regularization-- (= higher variance)
--- c3TourOfMLClassifiersInSklearn/perceptron.py
+++ c3TourOfMLClassifiersInSklearn/perceptron.py
@@ -6,6 +6,10 @@ from sklearn.metrics import accuracy_score
 from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                             '../'))
+from plot_decision_regions import plot_decision_regions
 
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
@@ -28,50 +32,6 @@ y_pred = ppn.predict(X_test_std)
 print('Misclassified samples: %d' %(y_test != y_pred).sum())
 print('Accuracy: %.4f' %accuracy_score(y_test, y_pred))
 
-
-def plot_decision_regions(
-    X, y, classifier, test_idx = None, resolution = 0.02):
-
-    # Set up marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'grey', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # plot all samples
-    X_test, y_test = X[test_idx, :], y[test_idx]
-
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x = X[y == cl, 0],
-                    y = X[y == cl, 1],
-                    alpha = 0.8,
-                    c = cmap(idx),
-                    marker = markers[idx],
-                    label = cl)
-
-    # highlight test samples
-    if test_idx:
-        X_test, y_test = X[test_idx, :], y[test_idx]
-        plt.scatter(X_test[:, 0],
-                    X_test[:, 1],
-                    c = '',
-                    alpha = 1.0,
-                    linewidth = 1,
-                    marker = 'o',
-                    s = 55,
-                    label = 'test set')
-                           
-        
 X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
 
