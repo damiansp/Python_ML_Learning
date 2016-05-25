f1a600a227f11344d3e33e50eee4cc61d71dd402
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-25 18:32:48 -0400

    Fixed indent error

--- c3TourOfMLClassifiersInSklearn/SVM.py
+++ c3TourOfMLClassifiersInSklearn/SVM.py
@@ -14,7 +14,7 @@ iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
 (X_train, X_test, y_train, y_test) = train_test_split(
-        X, y, test_size = 0.3, random_state = 0)
+    X, y, test_size = 0.3, random_state = 0)
 
 sc = StandardScaler()
 # Scale data
