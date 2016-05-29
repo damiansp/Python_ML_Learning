5f0e056388e71b6da219f6541cc777a1bac0d2a1
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-29 17:01:45 -0400

    Added more about NaN removal, and started info on imputing data

--- c4BuildingGoodTrainingSets_Preprocessing/missingData.py
+++ c4BuildingGoodTrainingSets_Preprocessing/missingData.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from io import StringIO
+from sklearn.preprocessing import Imputer
 
 csv_data = '''
 A,B,C,D
@@ -16,3 +17,33 @@ print df.isnull().sum()
 
 # df as numpy array:
 print df.values
+
+
+# Eliminate samples or features (rows or cols) with missing data
+# Simple but (1) at the cost of loss of info; (2) can introduce bias into data
+# if NAs are non-randomly distributed (as is often the case).
+
+# drop NaN rows
+print df.dropna()
+
+# drop NaN cols
+print df.dropna(axis = 1)
+
+# drop rows only if ALL cols ar NaN
+print df.dropna(how = 'all')
+
+# drop rows with n or more NaNs
+print df.dropna(thresh = 4)
+
+# drop rows only if NaN in specific column (here: 'C')
+print df.dropna(subset = ['C'])
+
+
+# Imputing missing values
+# Replace with (col) mean
+imp = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imp = imp.fit(df)
+imputed_data = imp.transform(df.values)
+print ""
+print "Imputed Data:"
+print imputed_data
