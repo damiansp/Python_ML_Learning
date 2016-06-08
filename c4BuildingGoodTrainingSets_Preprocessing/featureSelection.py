6288a4cbf79ae5454583f636063a901998c797d4
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-06-08 19:18:23 -0400

    Added Sequential Backward Selection class with examples

--- c4BuildingGoodTrainingSets_Preprocessing/featureSelection.py
+++ c4BuildingGoodTrainingSets_Preprocessing/featureSelection.py
@@ -1,9 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from itertools import combinations
+from sklearn.base import clone
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+
 
 wine = pd.read_csv(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
@@ -64,3 +70,87 @@ ax.legend(loc = 'upper center',
           ncol = 1,
           fancybox = True)
 plt.show()
+
+
+# Sequential Backward Selection (SBS) Algo
+class SBS():
+    def __init__(self,
+                 estimator,
+                 k_features,  # no. features desired
+                 scoring = accuracy_score,
+                 test_size = 0.25,
+                 random_state = 1):
+        self.scoring = scoring
+        self.estimator = clone(estimator)
+        self.k_features = k_features
+        self.test_size = test_size
+        self.random_state = random_state
+
+        
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+
+        return score
+
+        
+    def fit(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size = self.test_size, random_state = self.random_state)
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(
+            X_train, y_train, X_test, y_test, self.indices_)
+        self.scores_ = [score]
+
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+            for p in combinations(self.indices_, r = dim - 1):
+                score = self._calc_score(X_train, y_train, X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+
+            best = np.argmax(scores) 
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+            self.scores_.append(scores[best])
+
+        self.k_score_ = self.scores_[-1]
+
+        return self
+
+    
+    def transform(self, X):
+        return X[:, self.indices_]
+
+
+# Implement SBS using a knn classifier
+knn = KNeighborsClassifier(n_neighbors = 4)
+sbs = SBS(knn, k_features = 1)
+sbs.fit(X_train_std, y_train)
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, '-o')
+plt.ylim([0.7, 1.1])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.show()
+
+# Look at best 5 features
+k5 = list(sbs.subsets_[8])
+print(wine.columns[1:][k5])
+
+knn.fit(X_train_std, y_train)
+print('Training accuracy:', knn.score(X_train_std, y_train)) # 96.8
+print('Test accuracy:', knn.score(X_test_std, y_test)) # 94.4 ...slight overfit
+
+# See model with just the best 5 features
+knn.fit(X_train_std[:, k5], y_train)
+print('Training acc:', knn.score(X_train_std[:, k5], y_train)) # 96.0
+print('Test acc:', knn.score(X_test_std[:, k5], y_test)) # 98.1 :)
+
