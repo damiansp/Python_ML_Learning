e62c05fd6c4302a0454ddee20c61fa7251b7ee93
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-06-15 16:49:04 -0400

    added PCA transform and PC 1-2 plot

--- c5DimensionalityReduction/pca.py
+++ c5DimensionalityReduction/pca.py
@@ -10,9 +10,10 @@ wine = pd.read_csv(
 
 X, y = wine.iloc[:, 1:], wine.iloc[:, 0]
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size = 0.3, random_state = 0)
+    X, y, test_size = 0.3, random_state = 1)
 sc = StandardScaler()
-X_train_std = sc.fit_transform(X_test)
+X_train_std = sc.fit_transform(X_train)
+X_test_std  = sc.transform(X_test)
 
 # Construct covariance matrix
 cov_mat = np.cov(X_train_std.T)
@@ -37,3 +38,41 @@ plt.ylabel('Explained variance')
 plt.xlabel('Principal components')
 plt.legend(loc = 'best')
 plt.show()
+
+# Feature transformation to PC axes
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])\
+               for i in range(len(eigen_vals))]
+eigen_pairs.sort(reverse = True)
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
+
+#print('Matrix W:')
+#print(w)
+
+# transformed features x' = xW;
+# print(X_train_std[0].dot(w))
+# do to all data points:
+#print X_train_std
+X_train_pca = X_train_std.dot(w)
+#print X_train_pca # looks good
+
+# Vis
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+
+# print y_train # looks good
+# print X_train_pca[y_train == 1]
+
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    print 'l:', l
+    plt.scatter(X_train_pca[np.where(y_train == l), 0],
+                X_train_pca[np.where(y_train == l), 1],
+                c = c,
+                label = l,
+                marker = m)
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc = 'lower left')
+plt.show()
+               
