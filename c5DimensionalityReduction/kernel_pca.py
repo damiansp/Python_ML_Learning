247d55066f273b77bff560663025e0f68b6f0c6c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-06-27 17:35:12 -0400

    Added projection to (standard) PCs 1 & 2

--- c5DimensionalityReduction/kernel_pca.py
+++ c5DimensionalityReduction/kernel_pca.py
@@ -1,7 +1,9 @@
+import matplotlib.pyplot as plt
 import numpy as np
-#from scipy import exp
 from scipy.linalg import eigh
 from scipy.spatial.distance import pdist, squareform
+from sklearn.datasets import make_moons
+from sklearn.decomposition import PCA
 
 def rbf_kernel_pca(X, gamma, n_components):
     '''
@@ -40,3 +42,43 @@ def rbf_kernel_pca(X, gamma, n_components):
         eigvecs[:, -i] for i in range(1, n_components + 1)))
 
     return X_pc
+
+X, y = make_moons(n_samples = 100, random_state = 0)
+plt.scatter(
+    X[y == 0, 0], X[y == 0, 1], color = 'red', marker = '^', alpha = 0.5)
+plt.scatter(
+    X[y == 1, 0], X[y == 1, 1], color = 'blue', marker = 'o', alpha = 0.5)
+plt.show()
+
+
+# Project onto (standard) principal components
+pca = PCA(n_components = 2)
+X_pca = pca.fit_transform(X)
+
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (7, 3))
+ax[0].scatter(X_pca[y == 0, 0],
+              X_pca[y == 0, 1],
+              color = 'red',
+              marker = '^',
+              alpha = 0.5)
+ax[0].scatter(X_pca[y == 1, 0],
+              X_pca[y == 1, 1],
+              color = 'blue',
+              marker = 'o',
+              alpha = 0.5)
+ax[1].scatter(X_pca[y == 0, 0],
+              np.zeros((50, 1)) + 0.02,
+              color = 'red',
+              marker = '^',
+              alpha = 0.5)
+ax[1].scatter(X_pca[y == 1, 0],
+              np.zeros((50, 1)) - 0.02,
+              color = 'blue',
+              marker = 'o',
+              alpha = 0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.show()
