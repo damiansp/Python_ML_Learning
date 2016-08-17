a30f39eb95665f72371a6d83e4ffc8d449ea6aa2
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-08-17 17:43:50 -0400

    projection added

--- c5DimensionalityReduction/kernel_pca.py
+++ c5DimensionalityReduction/kernel_pca.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import FormatStrFormatter
+from scipy import exp
 from scipy.linalg import eigh
 from scipy.spatial.distance import pdist, squareform
 from sklearn.datasets import make_circles, make_moons
@@ -18,6 +19,7 @@ def rbf_kernel_pca(X, gamma, n_components):
     @return:
       X_pc: {NumPy ndarray}, shape = [n_samples, k_features]: projected data
         set
+      lambdas: list: Eigenvalues
     '''
 
     # Calculate pairwise squared Euclidean distances in the M x N dim data set
@@ -39,12 +41,53 @@ def rbf_kernel_pca(X, gamma, n_components):
     eigvals, eigvecs = eigh(K)
 
     # Collect the top k eigenvecs (projected samples)
-    X_pc = np.column_stack((
+    alphas = np.column_stack((
         eigvecs[:, -i] for i in range(1, n_components + 1)))
 
-    return X_pc
+    # Collect the corresponding eigenvals
+    lambdas = [eigvals[-i] for i in range(1, n_components + 1)]
 
-X, y = make_moons(n_samples = 100, random_state = 0)
+    return alphas, lambdas
+
+X, y = make_moons(n_samples = 100, random_state = 1103)
+alphas, lambdas = rbf_kernel_pca(X, gamma = 15, n_components = 1)
+x_new = X[25]
+x_proj = alphas[25] # orig projection
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+
+    return k.dot(alphas / lambdas)
+
+x_reproj = project_x(x_new, X, gamma = 15, alphas = alphas, lambdas = lambdas)
+
+plt.scatter(alphas[y == 0, 0],
+            np.zeros((50)),
+            color = 'red',
+            marker = '^',
+            alpha = 0.2)
+plt.scatter(alphas[y == 1, 0],
+            np.zeros((50)),
+            color = 'blue',
+            marker = 'o',
+            alpha = 0.2)
+plt.scatter(x_proj,
+            0,
+            color = 'black',
+            label = 'original projection of X[25]',
+            marker = '^',
+            s = 100)
+plt.scatter(x_reproj,
+            0,
+            color = 'green',
+            label = 'remapped X[25]',
+            marker = 'x',
+            s = 300)
+plt.legend(scatterpoints = 1)
+plt.show()
+
+
 plt.scatter(
     X[y == 0, 0], X[y == 0, 1], color = 'red', marker = '^', alpha = 0.5)
 plt.scatter(
