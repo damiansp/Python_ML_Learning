cde7ed6a984b3faf3bec3e3138848790104c8069
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-09-01 00:52:54 -0400

    added validation curve for variable parameter setting

--- c6ModelEvalAndHyperParamTuning/learning_and_validation_curves.py
+++ c6ModelEvalAndHyperParamTuning/learning_and_validation_curves.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-#from sklearn.cross_validation import cross_val_score, StratifiedKFold
 from sklearn.cross_validation import train_test_split
-#from sklearn.decomposition import PCA
-from sklearn.learning_curve import learning_curve
+from sklearn.learning_curve import learning_curve, validation_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, StandardScaler
@@ -65,5 +63,50 @@ plt.grid()
 plt.xlabel('Number of training samples')
 plt.ylabel('Accuracy')
 plt.legend(loc = 'lower right')
+plt.ylim([0.8, 1.0])
+plt.show()
+
+
+# Adressing Over/Underfitting with Validation Curves------------------
+param_range = [0.001, 0.01, 0.1, 1., 10., 100.]
+train_scores, test_scores = validation_curve(estimator = pipe_lr,
+                                             X = X_train,
+                                             y = y_train,
+                                             param_name = 'clf__C',
+                                             param_range = param_range,
+                                             cv = 10)
+train_mean = np.mean(train_scores, axis = 1)
+train_std  = np.std(train_scores,  axis = 1)
+test_mean  = np.mean(test_scores,  axis = 1)
+test_std   = np.std(test_scores,   axis = 1)
+
+plt.plot(param_range,
+         train_mean,
+         color = 'blue',
+         marker = 'o',
+         markersize = 5,
+         label = 'training accuracy')
+plt.fill_between(param_range,
+                 train_mean + train_std,
+                 train_mean - train_std,
+                 alpha = 0.15,
+                 color = 'blue')
+plt.plot(param_range,
+         test_mean,
+         color = 'red',
+         linestyle = '--',
+         marker = 's',
+         markersize = 5,
+         label = 'validation accuracy')
+plt.fill_between(param_range,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha = 0.15,
+                 color = 'red')
+plt.grid()
+plt.xscale('log')
+plt.legend(loc = 'lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
 plt.ylim([0.8, 1.0])
 plt.show()
