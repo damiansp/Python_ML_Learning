35be6bbb25bfbe8ce3417fd5750d5ae01fea5b73
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-09-01 13:13:45 -0400

    added grid search for param/hyperparam optimization

--- c6ModelEvalAndHyperParamTuning/learning_and_validation_curves.py
+++ c6ModelEvalAndHyperParamTuning/learning_and_validation_curves.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
 from sklearn.learning_curve import learning_curve, validation_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.svm import SVC
 
 # Load and preprocess data--------------------------------------------
 url = ('https://archive.ics.uci.edu/ml/machine-learning-databases/' +
@@ -110,3 +112,30 @@ plt.xlabel('Parameter C')
 plt.ylabel('Accuracy')
 plt.ylim([0.8, 1.0])
 plt.show()
+
+
+
+# Grid Search---------------------------------------------------------
+pipe_svc = Pipeline([('scl', StandardScaler()),
+                     ('clf', SVC(random_state = 6))])
+param_range = [0.0001, 0.001, 0.01, 0.1, 1., 10., 100., 1000.]
+param_grid = [{ 'clf__C': param_range,
+                'clf__kernel': ['linear'] },
+              { 'clf__C': param_range,
+                'clf__gamma': param_range,
+                'clf__kernel': ['rbf'] }]
+gs = GridSearchCV(estimator = pipe_svc,
+                  param_grid = param_grid,
+                  scoring = 'accuracy',
+                  cv = 10,
+                  n_jobs = -1)
+gs = gs.fit(X_train, y_train)
+
+print(gs.best_score_)
+print(gs.best_params_)
+
+# Use best params on test data
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+
+print('Test accuracy: %.3f' %clf.score(X_test, y_test))
